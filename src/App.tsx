e52e99b19674adf31e750cd013e6d2217b91d234
MrCorvoDev <mr.corvo.dev@gmail.com>
2025-03-10 14:16:17 +0400

    feat: Add Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Layout from './components/core/Layout';
 
 const About = lazy(() => import('./pages/About'));
+const Login = lazy(() => import('./pages/Login'));
 
 const App = () => (
    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
          <Route path='/' element={<Layout />}>
             <Route index element={<About />} />
+            <Route path='/login' element={<Login />} />
             <Route path='*' element={<About />} />
          </Route>
       </Routes>
